95ec89b817627788a4464a0ab275e36d686a3d2c
Pushpendra Kumar Dubey <114595912+Pushpendra6263@users.noreply.github.com>
2025-02-03 15:18:00 +0530

    new api.js

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,6 +1,6 @@
 export const sendMessage = async (message, apiKey) => {
 
-    const response = await fetch('https://pythonai-backend.netlify.app/chat', {
+    const response = await fetch('https://pythonai-backend.onrender.com/chat', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({ message, apiKey }),
